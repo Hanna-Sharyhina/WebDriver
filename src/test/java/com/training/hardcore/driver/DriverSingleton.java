a48f997b0040bd2957ddd83879bda58f5796976a
Hanna Sharyhina <hanna.konstantinovna@gmail.com>
2020-10-13 12:19:33 +0300

    Add break to the switch case.

--- src/test/java/com/training/hardcore/driver/DriverSingleton.java
+++ src/test/java/com/training/hardcore/driver/DriverSingleton.java
@@ -15,6 +15,7 @@ public class DriverSingleton {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
